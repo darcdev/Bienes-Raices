ceb55978a57cacf27734d567cf561c1693393a73
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2021-01-01 18:26:04 -0500

    seo y diseño layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react"
+import Layout from "../../components/Layout"
 
 const Index = () => {
-  return <h1>Index</h1>
+  return (
+    <Layout>
+      <h1>Index</h1>
+    </Layout>
+  )
 }
 
 export default Index
